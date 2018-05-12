f3d72eca5dd36e423add4118e25bdbf6a305aae9
sherwin29 <sherwin.chsh@yahoo.com>
2018-05-12 00:14:47 -0700

    add graph text pos; add user-defined label name

--- src/vis.py
+++ src/vis.py
@@ -1,20 +1,24 @@
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 import math
-
 
 # matplotlib.rcParams.update({'figure.autolayout': True})
 
 def info_dict_preprocess(info_dict):
     info_dict.setdefault('draw_node_size', 10)
     info_dict.setdefault('draw_node_label_enable', True)
+    info_dict.setdefault('node_label_name', '')
     info_dict.setdefault('draw_node_label_font_size', 6)
 
     info_dict.setdefault('draw_edge_label_enable', False)
+    info_dict.setdefault('edge_label_name', '')
     info_dict.setdefault('draw_edge_label_font_size', 6)
 
-    info_dict.setdefault('each_graph_font_size', "")
+    info_dict.setdefault('each_graph_text_font_size', "")
+    info_dict.setdefault('each_graph_text_pos', [0.5, 0.8])
 
     info_dict.setdefault('plot_dpi', 200)
     info_dict.setdefault('plot_save_path', "")
@@ -28,13 +32,6 @@ def calc_subplot_size(area):
     return [h, w]
 
 
-def draw_extra(i, ax, info_dict, text):
-    pass
-    ax.title.set_position([0.5, 0.98])
-    ax.set_title(text, fontsize=info_dict['each_graph_font_size'])
-    plt.axis('off')
-
-
 def list_safe_get(l, index, default):
     try:
         return l[index]
@@ -42,31 +39,39 @@ def list_safe_get(l, index, default):
         return default
 
 
+def draw_extra(i, ax, info_dict, text):
+    pass
+    left = list_safe_get(info_dict['each_graph_text_pos'], 0, 0.5)
+    bottom = list_safe_get(info_dict['each_graph_text_pos'], 1, 0.8)
+    print(left, bottom)
+    ax.title.set_position([left, bottom])
+    ax.set_title(text, fontsize=info_dict['each_graph_text_font_size'])
+    plt.axis('off')
+
+
+
 def draw_graph(g, info_dict):
     if g is None:
         return
     pos = graphviz_layout(g)
-
+    
     node_labels = nx.get_node_attributes(g, 'type')
-    color_values = [info_dict['draw_node_color_map'].get(node_label, 'yellow')
-                    for node_label in node_labels.values()]
-    nx.draw_networkx(g, pos, node_color=color_values, with_labels=False,
-                     node_size=info_dict['draw_node_size'])
+    color_values = [info_dict['draw_node_color_map'].get(node_label, 'yellow') for node_label in node_labels.values()]
+    nx.draw_networkx(g, pos, node_color=color_values, with_labels=False, node_size=info_dict['draw_node_size'])
 
     if info_dict['draw_node_label_enable'] == True:
-        node_labels = nx.get_node_attributes(g, 'type')
-
-        nx.draw_networkx_labels(g, pos, node_labels, font_size=info_dict[
-            'draw_node_label_font_size'])
-
+        node_labels = nx.get_node_attributes(g, info_dict['node_label_name'])
+        
+        nx.draw_networkx_labels(g, pos, node_labels, font_size=info_dict['draw_node_label_font_size'])
+    
     if info_dict['draw_edge_label_enable'] == True:
-        edge_labels = nx.get_edge_attributes(g, 'valence')
-        nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels,
-                                     font_size=info_dict[
-                                         'draw_edge_label_font_size'])
+        edge_labels = nx.get_edge_attributes(g, info_dict['edge_label_name'])
+        nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels, font_size=info_dict['draw_edge_label_font_size'])
+
 
 
 def vis(q=None, gs=None, info_dict=None):
+
     info_dict_preprocess(info_dict)
 
     # get num
@@ -76,32 +81,29 @@ def vis(q=None, gs=None, info_dict=None):
     # draw query graph
     ax = plt.subplot(plot_m, plot_n, 1)
     draw_graph(q, info_dict)
-    draw_extra(0, ax, info_dict,
-               list_safe_get(info_dict['each_graph_text_list'], 0, ""))
+    draw_extra(0, ax, info_dict, list_safe_get(info_dict['each_graph_text_list'], 0, ""))
 
     # draw graph candidates
     for i in range(len(gs)):
         ax = plt.subplot(plot_m, plot_n, i + 2)
         draw_graph(gs[i], info_dict)
-        draw_extra(i, ax, info_dict,
-                   list_safe_get(info_dict['each_graph_text_list'], i + 1, ""))
+        draw_extra(i, ax, info_dict, list_safe_get(info_dict['each_graph_text_list'], i + 1, ""))
 
     # plot setting
     # plt.tight_layout()
-    left = 0.01  # the left side of the subplots of the figure
-    right = 0.99  # the right side of the subplots of the figure
-    bottom = 0.05  # the bottom of the subplots of the figure
-    top = 0.95  # the top of the subplots of the figure
-    wspace = 0.02  # the amount of width reserved for blank space between subplots
-    hspace = 0.16  # the amount of height reserved for white space between subplots
-    plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top,
-                        wspace=wspace, hspace=hspace)
+    left  = 0.01  # the left side of the subplots of the figure
+    right = 0.99    # the right side of the subplots of the figure
+    bottom = 0.05   # the bottom of the subplots of the figure
+    top = 0.95      # the top of the subplots of the figure
+    wspace = 0.02   # the amount of width reserved for blank space between subplots
+    hspace = 0.16   # the amount of height reserved for white space between subplots
+    plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
 
     # save / display
     save_path = info_dict['plot_save_path']
     if save_path is None or save_path == "":
         plt.show()
     else:
-        sp = info_dict['plot_save_path']
-        print('Saving qeury vis plot to {}'.format(sp))
-        plt.savefig(sp, dpi=info_dict['plot_dpi'])
+        plt.savefig(info_dict['plot_save_path'], dpi=info_dict['plot_dpi'])
+        
+
